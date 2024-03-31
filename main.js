4d4b0ea3266e9ff241403fd39ea276be9181c9a5
Qiratsumra <saeedqirat43@gmail.com>
2024-03-31 20:43:25 +0500

    cli todo list

---
+++ main.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let toDos = [];
+let condition = true;
+// use loop for repeatively 
+while (condition) {
+    let answers = await inquirer.prompt([
+        { type: "input",
+            name: "todo",
+            message: "What you want to add your todos"
+        },
+        { type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more",
+            default: "false"
+        }
+    ]);
+    let { todo, addmore } = answers;
+    console.log(answers);
+    condition = addmore;
+    if (todo) {
+        toDos.push(todo);
+    }
+    else {
+        console.log("Kindly add input");
+    }
+    if (toDos.length > 0) {
+        console.log("Your todo list:");
+        toDos.forEach(todo => console.log(todo));
+    }
+    else {
+        console.log("No to found");
+    }
+}
